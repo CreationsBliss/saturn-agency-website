39a7f2db136a695027a4f7fce94f653a8fd1c7e2
Musfakur <procreationsbliss@gmail.com>
2023-03-07 23:38:31 +0600

    portfolio area done

--- js/app.js
+++ js/app.js
@@ -7,16 +7,37 @@ $(document).ready(function(){
     smartSpeed: 1000,
     navText: ['PREV', 'NEXT'],
     dots: false,
-    loop:true,
     nav:true,
     items: 1,
     responsive:{
         0:{
+          nav:false,
         },
-        600:{
+        768:{
+          nav:true,
         },
-        1000:{
-        }
     }
-})
+});
+
+  $('#projects').owlCarousel({
+    loop:true,
+    margin:20,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 1000,
+    dots: true,
+    items: 3,
+    responsive:{
+        0:{
+          items: 1,
+        },
+        768:{
+          items: 2,
+        },
+        992:{
+          items: 2,
+          center: true,
+        },
+    }
+  })
 })
